23e32c4f0c2ad3c2f9268e6ea552efb6d607130e
Amrit Sah <sahamrit3333@gmail.com>
2026-02-14 10:38:07 +0530

    Fix: Remove lingering import from deleted config.js

--- client/src/pages/AdminLogin.jsx
+++ client/src/pages/AdminLogin.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { FaLock, FaArrowLeft } from 'react-icons/fa';
-import API_URL from '../config';
 
 const AdminLogin = () => {
     const [secret, setSecret] = useState('');
